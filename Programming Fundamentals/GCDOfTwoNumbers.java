b025b5b24cc85be054c35ad3c72c8141c5fe7343
hemanthsavasere <hemanth.savasere@gmail.com>
2026-06-20 10:32:55 +0000

    GCD of 2 numbers correction

--- Programming Fundamentals/GCDOfTwoNumbers.java
+++ Programming Fundamentals/GCDOfTwoNumbers.java
@@ -4,15 +4,10 @@ public class GCDOfTwoNumbers {
         System.out.println(gcd(5, 11));
         System.out.println(gcd(15, 10));
         System.out.println(gcd(4, 18));
+        System.out.println(gcd(0, 18));
     }
 
     static int gcd (int num1, int num2){
-        
-        if (num1 > num2) {
-            int temp = num1;
-            num1 = num2;
-            num2 = temp;
-        }
 
         while(num1 != 0){
             int rem = num2 % num1;
