82594197cf725456cd11387ba1320cd704538f6b
hemanthsavasere <hemanth.savasere@gmail.com>
2026-06-20 10:17:58 +0000

    GCD of Two numbers

---
+++ Programming Fundamentals/GCDOfTwoNumbers.java
@@ -0,0 +1,43 @@
+public class GCDOfTwoNumbers {
+    public static void main(String[] args) {
+        System.out.println(gcd(5, 10));
+        System.out.println(gcd(5, 11));
+        System.out.println(gcd(15, 10));
+        System.out.println(gcd(4, 18));
+    }
+
+    static int gcd (int num1, int num2){
+        
+        if (num1 > num2) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+        while(num1 != 0){
+            int rem = num2 % num1;
+            num2 = num1;
+            num1 = rem;
+        }
+
+        return num2;
+    }
+
+}
+
+/*
+    5, 10
+    
+
+    11, 17
+    6, 11
+    5, 6
+    1, 5
+    0, 1 
+
+
+
+    10, 5
+    4, 10;
+
+*/
